53afacd7a0a94aad5e3cb5b6e555d3dc99b1dc20
Raghu Reddy <raghureddy.ram@gmail.com>
2017-01-11 14:40:21 -0800

    1-7 catch of the day

---
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -0,0 +1,18 @@
+// let's go!
+import React from 'react';
+
+
+class StorePicker extends React.Component {
+    render(){
+        return (
+            <form className="store-selector">
+            
+                <h2>Please enter a store</h2>
+                <input type="text" requred placeholder="Store Name" />
+                <button type="submit">Visit Store</button>
+            </form>
+        )
+    }
+}
+
+export default StorePicker;
