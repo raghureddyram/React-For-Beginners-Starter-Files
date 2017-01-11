a8c1be04c9e77f2a647a08d4512b878efbc0a1cb
Raghu Reddy <raghureddy.ram@gmail.com>
2017-01-11 15:52:37 -0800

    adding BrowserRouter

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,23 @@
 // let's go!
 import React from 'react';
 import {render} from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 import './css/style.css'
 import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
+// Match compnents can't be direct children of BrowserRouter, wrap in a div
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div> 
+        <Match exactly pattern="/" component={StorePicker}/>
+        <Match exactly pattern="/store/:storeId" component={App}/>
+        <Miss  component={NotFound}/>
+      </div>
+    </BrowserRouter>
+  )
+}
 
-
-render(<App/>, document.querySelector('#main'));
+render(<Root/>, document.querySelector('#main'));
