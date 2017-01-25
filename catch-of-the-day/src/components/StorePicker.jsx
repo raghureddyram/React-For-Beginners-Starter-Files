1feefcc48767635cf74291df548335400fb8dc40
Raghu Reddy <raghureddy.ram@gmail.com>
2017-01-25 15:49:15 -0800

    Add ref for input using a function, bind goToStore function to component

--- catch-of-the-day/src/components/StorePicker.jsx
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -1,14 +1,28 @@
 // let's go!
 import React from 'react';
+import { getFunName } from '../helpers';
 
 
 class StorePicker extends React.Component {
+    // constructor(){
+    //   super()
+    //   this.goToStore = this.goToStore.bind(this);
+    // }
+
+    goToStore(event){
+      event.preventDefault();
+      console.log('you changed the url');
+      console.log(this.storeInput.value)
+    }
+
+
+
     render(){
         return (
-            <form className="store-selector">
-            
+            <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+
                 <h2>Please enter a store</h2>
-                <input type="text" requred placeholder="Store Name" />
+                <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } ref={(input) => {this.storeInput = input}}/>
                 <button type="submit">Visit Store</button>
             </form>
         )
