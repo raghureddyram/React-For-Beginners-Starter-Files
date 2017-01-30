ac79303683b46d968c859366f36396306033f08f
Raghu Reddy <raghureddy.ram@gmail.com>
2017-01-29 19:41:44 -0800

    Pass state down, access via props

--- catch-of-the-day/src/components/App.jsx
+++ catch-of-the-day/src/components/App.jsx
@@ -5,6 +5,31 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends React.Component{
+  constructor(){
+    super();
+
+    this.addFish = this.addFish.bind(this);
+
+    this.state = {
+      fishes: {},
+      order: {},
+    }
+  }
+
+  addFish(fish){
+    //update our state
+    // do that by taking copy of current state, then update. for performance
+
+    const fishes = {...this.state.fishes};
+    // add in our new fish
+    const timestamp = Date.now();
+
+    fishes[`fish-${timestamp}`] = fish;
+
+    this.setState({ fishes: fishes }); // set the updated state only for fish es
+    // set state
+  }
+
     render(){
         return(
             <div className="catch-of-the-day">
@@ -12,7 +37,7 @@ class App extends React.Component{
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish}/>
             </div>
         )
     }
--- catch-of-the-day/src/components/Inventory.jsx
+++ catch-of-the-day/src/components/Inventory.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component{
     render(){
         return(
+          <div>
             <p>Inventory</p>
+            <AddFishForm addFish={this.props.addFish}/>
+          </div>
         )
     }
 }
