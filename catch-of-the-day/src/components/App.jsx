c615265758fc45981c0e66afd31e20bade6e2fe8
Raghu Reddy <raghureddy.ram@gmail.com>
2017-03-13 22:42:01 -0700

    Update react-scripts

--- catch-of-the-day/src/components/App.jsx
+++ catch-of-the-day/src/components/App.jsx
@@ -3,12 +3,13 @@ import StorePicker from './StorePicker';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
-
+import sampleFishes from '../sample-fishes';
 class App extends React.Component{
   constructor(){
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
 
     this.state = {
       fishes: {},
@@ -30,6 +31,12 @@ class App extends React.Component{
     // set state
   }
 
+  loadSamples(){
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
     render(){
         return(
             <div className="catch-of-the-day">
@@ -37,7 +44,7 @@ class App extends React.Component{
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish}/>
+                <Inventory loadSamples={this.loadSamples} addFish={this.addFish}/>
             </div>
         )
     }
--- catch-of-the-day/src/components/Inventory.jsx
+++ catch-of-the-day/src/components/Inventory.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component{
+
     render(){
         return(
           <div>
             <p>Inventory</p>
             <AddFishForm addFish={this.props.addFish}/>
+            <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
           </div>
         )
     }
